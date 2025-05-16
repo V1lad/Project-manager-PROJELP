f66020de68f855ffff6c72ad118af1551be230b9
Петров Владислав Дмитриевич <petrovvd@student.bmstu.ru>
2025-05-16 17:36:19 +0300

    Refactored has_access function

--- Web/functions.py
+++ Web/functions.py
@@ -5,10 +5,6 @@ from .models import Project, SubProject, Note
 def has_access_to_project(user, project):
     if project.owner_id == user.id:
         return True
-    else:
-        allowed_users = loads(project.allowedUsers)
-        if str(user.id) in allowed_users:
-            return True
-        else:
-            return False
-        
+
+    allowed_users = loads(project.allowedUsers)
+    return str(user.id) in allowed_users
