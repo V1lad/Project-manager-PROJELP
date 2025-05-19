351a853097ca53ff8c5f59a4b940f85ecda3f298
Петров Владислав Дмитриевич <petrovvd@student.bmstu.ru>
2025-05-20 02:01:04 +0300

    added get_date

--- Web/functions.py
+++ Web/functions.py
@@ -14,3 +14,9 @@ def get_time(time_str):
     # 2025-05-16-18-21 --> 16:18 16.05.2025
     dates = time_str.split("-")
     return f"{dates[3]}:{dates[4]} {'.'.join(dates[0:3][::-1])}"
+
+# Приводит дату из БД к виду для показа на странице
+def get_date(time_str):
+    # 2025-05-16 --> 16.05.2025
+    dates = time_str.split("-")
+    return f"{'.'.join(dates[0:3][::-1])}"
