23ec7760b27fce53d911c808542e7021d25a8573
Петров Владислав Дмитриевич <petrovvd@student.bmstu.ru>
2025-05-21 01:40:52 +0300

    Fixed has_access and added has_red_access

--- Web/functions.py
+++ Web/functions.py
@@ -6,8 +6,24 @@ def has_access_to_project(user, project):
     if project.owner_id == user.id:
         return True
 
+    if user.is_admin == "True":
+        return True
+    
     allowed_users = loads(project.allowedUsers)
-    return str(user.id) in allowed_users
+    return [str(user.id), 0] in allowed_users or [str(user.id), 1] in allowed_users
+
+# Возвращает True если у пользователя есть доступ к редактированию проекта
+def has_redact_rights_to_project(user, project):
+    if project.owner_id == user.id:
+        return True
+
+    if user.is_admin == "True":
+        return True
+    
+    allowed_users = loads(project.allowedUsers)
+    return [str(user.id), 1] in allowed_users
+
+
 
 # Приводит время из БД к виду для показа на странице
 def get_time(time_str):
